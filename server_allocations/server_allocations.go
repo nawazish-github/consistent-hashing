77ce7731ebf31b643243c18a39b16f956c9e0280
Mohammad Nawazish Khan <md.nawaish.khan@gmail.com>
2021-12-11 01:18:17 +0530

    Add logic to search the next available server for the given request

--- server_allocations/server_allocations.go
+++ server_allocations/server_allocations.go
@@ -1,6 +1,10 @@
 package server_allocations
 
-import "github.com/nawazish-github/consistent-hashing/hash"
+import (
+	"fmt"
+
+	"github.com/nawazish-github/consistent-hashing/hash"
+)
 
 type ServerAllocation struct {
 	serverLocations map[int64]string
@@ -16,5 +20,22 @@ func (sa *ServerAllocation) AllocateServer(serverKey string) {
 }
 
 func (sa *ServerAllocation) FindTheServer(reqKey string) {
+	loc := hash.LocationOnRing(reqKey)
+	if server, ok := sa.serverLocations[loc]; ok {
+		fmt.Printf("Request %s is served by server %s", reqKey, server)
+	} else {
+		sa.walk(loc, reqKey)
+	}
+}
 
+func (sa *ServerAllocation) walk(loc int64, reqKey string) string {
+
+	//15, 30, 45, 60, 67, 88, 90 | 58
+
+	sortedKeys := make([]int64, len(sa.serverLocations))
+
+	for k, _ := range sa.serverLocations {
+		sortedKeys = append(sortedKeys, k)
+	}
+	return ""
 }
