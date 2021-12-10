bcc3b55a7cc6194a9d080a46210926bf6803ec82
Mohammad Nawazish Khan <md.nawaish.khan@gmail.com>
2021-12-11 00:09:49 +0530

    Register server on the consistent hash ring

---
+++ server_allocations/server_allocations.go
@@ -0,0 +1,16 @@
+package server_allocations
+
+import "github.com/nawazish-github/consistent-hashing/hash"
+
+type ServerAllocation struct {
+	serverLocations map[int64]string
+}
+
+func (sa *ServerAllocation) InitServerAllocation() {
+	sa.serverLocations = make(map[int64]string)
+}
+
+func (sa *ServerAllocation) AllocateServer(serverKey string) {
+	loc := hash.LocationOnRing(serverKey)
+	sa.serverLocations[loc] = serverKey
+}
